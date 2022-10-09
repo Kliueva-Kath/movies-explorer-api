9954e399e791538aa5d313118aa7a9479a0a1c02
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-10-09 22:01:58 +0300

    feat: create models

---
+++ models/movie.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const { urlRegExp } = require('../utils/regExp');
+
+const cardSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegExp.test(v);
+      },
+      message: 'Ошибка в url-адресе',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegExp.test(v);
+      },
+      message: 'Ошибка в url-адресе',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegExp.test(v);
+      },
+      message: 'Ошибка в url-адресе',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'user',
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', cardSchema);
---
+++ models/user.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        return validator.isEmail(v);
+      },
+      message: 'Пожалуйста, проверьте правильность ввода',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+});
+
+userSchema.methods.toJSON = function removePassword() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+module.exports = mongoose.model('user', userSchema);
