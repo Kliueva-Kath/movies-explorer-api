f2445ff5e386e5617214df48c7de1d41234bb2be
Kliueva-Kath <98223602+Kliueva-Kath@users.noreply.github.com>
2022-11-05 06:31:52 +0300

    fix: added allowedCors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,23 +1,25 @@
 const allowedCors = [
-  'http://api.movies.kliueva.nomoredomains.icu',
-  'https://api.movies.kliueva.nomoredomains.icu',
-  'http://localhost:3000',
+  "http://movies.kliueva.nomoredomains.icu",
+  "https://movies.kliueva.nomoredomains.icu",
+  "http://api.movies.kliueva.nomoredomains.icu",
+  "https://api.movies.kliueva.nomoredomains.icu",
+  "http://localhost:3000",
 ];
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers["access-control-request-headers"];
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
   }
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+  if (method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    res.header("Access-Control-Allow-Headers", requestHeaders);
 
     return res.end();
   }
